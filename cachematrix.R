fe2520d5512405ea2d516fa7d1e40137a75cfd03
chaimingh <chaiming.h@gmail.com>
2024-06-02 10:31:11 -0400

    Caching the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Functions makeCacheMatrix() and cacheSolve() are working together to avoid 
+# recomputing an inverse of a matrix and this is achieved by caching.
 
-## Write a short comment describing this function
 
+# makeCacheMatrix() takes a square invertible matrix 'x' as an input and returns
+# a "special matrix" which is a list containing functions. 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInvMatrix <- function(invMat) m <<- invMat
+  getInvMatrix <- function() m
+  list(set = set, get = get, 
+       setInvMatrix = setInvMatrix, 
+       getInvMatrix = getInvMatrix)
 
 }
 
-
-## Write a short comment describing this function
-
+# cacheSolve() solves for an inverse of a matrix that is stored inside the 
+# "special matrix" created by makeCacheMatrix().
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInvMatrix()
+  if(!is.null(m)){
+    message("Getting cached matrix")
+    return(m)
+  }
+  data <-x$get()
+  m <- solve(data,...)
+  x$setInvMatrix(m)
+  m
 }
+
+#-------------------------------------------------------------------------------
+# Testing:
+
+# 1. Create a 2x2 matrix 'a' with random data.
+x0 <- rnorm(4)
+dim(x0) <- c(2,2)
+
+# 2. Make a special matrix 'xs'
+x <- makeCacheMatrix(x0)
+cacheSolve(x)
